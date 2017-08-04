6204366b39cd7cb9dee05f60e34be1794364b10e
Andrea Mansur <apmansur1993@gmail.com>
2017-08-04 02:29:45 +0000

    render paddles, net, and ball using js

--- app/scripts/pong.js
+++ app/scripts/pong.js
@@ -1,38 +1,78 @@
-    var canvas = document.getElementById("pong-table");
-    var context = canvas.getContext('2d');
+var canvas = document.getElementById("pong-table");
+var context = canvas.getContext('2d');
+
+
+function Paddle(x,y) {
+    this.x = x;
+    this.y = y;
+    this.width = 25;
+    this.height = 100;
+}
+
+
+function Player(){
+    var paddle = new Paddle(0, 50);
+    paddle.render()
+}
+
+function Computer(){
+    var paddle = new Paddle(canvas.width-50, 50 );
+    paddle.render()
+}
+
+function Ball(){
+          this.x = canvas.width / 2;
+          this.y = canvas.height / 2;
+          this.radius = 15;
+          this.startAngle = 1 * Math.PI;
+          this.endAngle = 0.9999999 * Math.PI;
+          this.counterClockwise = false;
+}
+
+
+
+function Net(){
+        this.x = canvas.width / 2;
+        this.p1 = 10;
+        this.p2 = canvas.height - 10
+}
+
+
+// render functions
+
+    Net.prototype.render = function(){
           context.beginPath();
-          context.rect(0, 50, 25, 100);
-          context.fillStyle = 'black';
-          context.fill();
-          context.lineWidth = 1;
-          context.strokeStyle = 'black';
-          context.stroke();
-                  
-          context.beginPath();
-          context.rect(950, 50 , 25, 100);
-          context.fillStyle = 'black';
-          context.fill();
-          context.lineWidth = 1;
-          context.strokeStyle = 'black';
-          context.stroke();
-          
-          context.beginPath();
-          context.moveTo(500,10);
-          context.lineTo(500,450);
+          context.moveTo(canvas.width / 2,this.p1);
+          context.lineTo(canvas.width / 2,this.p2);
           context.strokeStyle = "black";
           context.stroke();
-                  
-          var x = canvas.width / 2;
-          var y = canvas.height / 2;
-          var radius = 15;
-          var startAngle = 1 * Math.PI;
-          var endAngle = 0.9999999 * Math.PI;
-          var counterClockwise = false;
-    
+    }
+    Ball.prototype.render = function (){
           context.beginPath();
-          context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+          context.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle, this.counterClockwise);
           context.lineWidth = 1;
-    
-          // line color
           context.strokeStyle = 'black';
           context.stroke();
+    }
+   Paddle.prototype.render = function(){
+          context.beginPath();
+          context.rect(this.x, this.y, this.width, this.height);
+          context.fillStyle = 'black';
+          context.fill();
+          context.lineWidth = 1;
+          context.strokeStyle = 'black';
+          context.stroke();
+    }
+
+window.onload = function(){
+  var player = Player();
+  var computer = Computer();
+  var net = new Net();
+  var ball = new Ball();
+  
+  player;
+  computer;
+  net.render();
+  ball.render();
+
+}
