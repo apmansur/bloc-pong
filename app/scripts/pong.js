abc1e85e94cccb79718e6b8e727bf1aaefe81b82
Andrea <apmansur1993@gmail.com>
2017-08-08 22:10:51 -0500

    refactor moving paddles

--- app/scripts/pong.js
+++ app/scripts/pong.js
@@ -1,122 +1,178 @@
-var canvas = document.getElementById("pong-table");
-var context = canvas.getContext('2d');
+    var canvas = document.getElementById("pong-table");
+    var context = canvas.getContext('2d');
+    context.fillStyle = "white";
+    var game = new Game();
+  
+
+    Game.prototype.update = function () {
+        if (this.paused)
+            return;
+        if (this.keys.isPressed(83)) { // DOWN
+            this.player1.paddle.y = Math.min(canvas.height - this.player1.paddle.height, this.player1.paddle.y + 4);
+        } else if (this.keys.isPressed(87)) { // UP
+            this.player1.paddle.y = Math.max(0, this.player1.paddle.y - 4);
+        }
+
+        if (this.keys.isPressed(40)) { // DOWN
+            this.computer.paddle.y = Math.min(canvas.height - this.computer.paddle.height, this.computer.paddle.y + 4);
+        } else if (this.keys.isPressed(38)) { // UP
+            this.computer.paddle.y = Math.max(0, this.computer.paddle.y - 4);
+        }
+    }
+    
+   function KeyListener() {
+        this.pressedKeys = [];
+        this.keydown = function (event) {
+            this.pressedKeys[event.keyCode] = true;
+        };
+        this.keyup = function (event) {
+            this.pressedKeys[event.keyCode] = false;
+        };
+        document.addEventListener("keydown", this.keydown.bind(this));
+        document.addEventListener("keyup", this.keyup.bind(this));
+    };
+
+    KeyListener.prototype.isPressed = function (key) {
+        return this.pressedKeys[key] ? true : false;
+    };
+
+    KeyListener.prototype.addKeyPressListener = function (keycode, callback) {
+        document.addEventListener("keypress", function (event) {
+            if (event.keyCode == keyCode)
+                callback(event);
+        });
+    };
 
 
-function Paddle(x, y) {
-    this.x = x;
-    this.y = y;
-    this.width = 25;
-    this.height = 100;
-    this.speed = 3 px;
-};
 
-Paddle.prototype.move = function () {
-    if ( /*down key is pressed key code keypress.key = 40*/ ) {
-        if (this.y > /*the start of canvas*/ ) {
-            this.y =- this.speed;
-        } else {
-            this.y = this.y
-         } 
-    else if ( /*up key is pressed key code keypress.key = 38*/ ) {
-            if (this.y < canvas.height) {
-                this.y = +this.speed
+    function Paddle(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = 25;
+        this.height = 100;
+        this.speed = 3;
+        this.score = 0;
+    };
+
+//combined in game.update()
+
+   /* Paddle.prototype.move = function (x, y) {
+        this.x += x;
+        this.y += y;
+        if (this.y < 50) {
+            this.y = 50;
+        } else if (this.y > (canvas.height - 50)) {
+            this.y = canvas.height - 50;
+        }
+    };
+
+
+    Player.prototype.move = function () {
+        for (key in keyDown) {
+            console.log(key);
+            if (key == "40") {
+                this.paddle.move(0, 3);
+            } else if (key == "38") {
+                this.paddle.move(0, -3);
             }
         }
     }
-};
 
-function Player() {
-    var paddle = new Paddle(0, 50);
-    paddle.render()
-};
-
-function Computer() {
-    var paddle = new Paddle(canvas.width - 50, 50);
-    paddle.render()
-};
-
-function Ball() {
-    this.x = canvas.width / 2;
-    this.y = canvas.height / 2;
-    this.radius = 15;
-    this.startAngle = 1 * Math.PI;
-    this.endAngle = 0.9999999 * Math.PI;
-    this.counterClockwise = false;
-};
-
-
-function Net() {
-    this.x = canvas.width / 2;
-    this.p1 = 10;
-    this.p2 = canvas.height - 10
-};
+    Computer.prototype.move = function () {
+        for (key in keyDown) {
+            if (key == "40") {
+                this.paddle.move(0, 3);
+            } else if (key == "38") {
+                this.paddle.move(0, -3);
+            }
+        }
+    }
+*/
+    
+    //constructors
+    
+    function Game(){
+        this.player1 = new Player();
+        this.computer = new Computer();
+        this.net = new Net();
+        this.ball = new Ball();
+        this.keys = new KeyListener();
+    }
 
 
-// render functions
-
-Net.prototype.render = function () {
-    context.beginPath();
-    context.moveTo(canvas.width / 2, this.p1);
-    context.lineTo(canvas.width / 2, this.p2);
-    context.strokeStyle = "black";
-    context.stroke();
-};
-Ball.prototype.render = function () {
-    context.beginPath();
-    context.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle, this.counterClockwise);
-    context.lineWidth = 1;
-    context.strokeStyle = 'black';
-    context.stroke();
-};
-Paddle.prototype.render = function () {
-    context.beginPath();
-    context.rect(this.x, this.y, this.width, this.height);
-    context.fillStyle = 'black';
-    context.fill();
-    context.lineWidth = 1;
-    context.strokeStyle = 'black';
-    context.stroke();
-};
-
-//aniamtion
-
-var startTime = -1;
-var animationLength = 2000;
-var animate = window.requestAnimationFrame ||
-    function (step) {
-        window.setTimeout(step, 1000 / 60)
+    function Player() {
+        this.paddle = new Paddle(0, 50);
     };
 
-function step(timestamp) {
-    // Calculate animation progress
-    var progress = 0;
+    function Computer() {
+        this.paddle = new Paddle(canvas.width - 50, 50);
+    };
 
-    if (startTime < 0) {
-        startTime = timestamp;
-    } else {
-        progress = timestamp - startTime;
+    function Ball() {
+        this.x = canvas.width / 2;
+        this.y = canvas.height / 2;
+        this.radius = 15;
+        this.startAngle = 1 * Math.PI;
+        this.endAngle = 0.9999999 * Math.PI;
+        this.counterClockwise = false;
+    };
+
+
+    function Net() {
+        this.x = canvas.width / 2;
+        this.p1 = 10;
+        this.p2 = canvas.height - 10
+    };
+
+
+    // render functions
+
+    Net.prototype.render = function () {
+        context.beginPath();
+        context.moveTo(canvas.width / 2, this.p1);
+        context.lineTo(canvas.width / 2, this.p2);
+        context.strokeStyle = "black";
+        context.stroke();
+    };
+    Ball.prototype.render = function () {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle, this.counterClockwise);
+        context.lineWidth = 1;
+        context.strokeStyle = 'black';
+        context.stroke();
+    };
+    Paddle.prototype.render = function () {
+        context.beginPath();
+        context.rect(this.x, this.y, this.width, this.height);
+        context.fillStyle = 'black';
+        context.fill();
+        context.lineWidth = 1;
+        context.strokeStyle = 'black';
+        context.stroke();
+    };
+
+
+    //animation
+
+    /*var startTime = -1;
+    var animationLength = 500;
+    var animate = window.requestAnimationFrame ||
+        function (step) {
+            window.setTimeout(step, 1000 / 60)
+        };
+
+    function step() {
+
+        game.update();
+        setTimeout(step, 1000 / 60);
     }
+    */
 
-    // Do animation ...
-    if (progress < animationLength) {
-        requestAnimationFrame(doAnimation);
+    window.onload = function () {
+        game.player1.paddle.render();
+        game.computer.paddle.render();
+        game.net.render();
+        game.ball.render();
+        step();
+
     }
-}
-
-window.requestAnimationFrame(step).addEventListener(/*keypress*/);
-}
-
-//asynchronous key input??
-
-window.onload = function () {
-    var player = Player();
-    var computer = Computer();
-    var net = new Net();
-    var ball = new Ball();
-
-    player;
-    computer;
-    net.render();
-    ball.render();
-
-}
